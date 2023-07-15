4ac4233574d4140f910aaf953a30e73d73080adb
kaapaS <127042824+kaapaS@users.noreply.github.com>
2023-07-15 22:55:32 +0530

    Create mytoken.sol

---
+++ contract/mytoken.sol
@@ -0,0 +1,47 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.18;
+
+contract MyToken {
+
+    // public variables here
+    string public TokenName= "Ethereum";
+    string public TokenAbbrv= "ETH";
+    uint public TotalSupply=0;
+    address owner;
+
+
+    // mapping variable here
+    mapping(address=>uint) public bal;
+    constructor(){
+        owner= msg.sender;
+    }
+
+    function getBal() external view returns(uint){
+        return bal[msg.sender];
+    }
+
+    // mint function
+    function mint (address _address, uint _value)public{
+        require(msg.sender == owner, "token can onlly be min by owner");
+        TotalSupply += _value;
+        bal[_address] += _value;
+
+    }
+
+    //transfer function
+    function transfer (address _address, uint _value)public{
+        require(bal[msg.sender] >= _value, "Balance is not sufficient !!");
+        TotalSupply += _value;
+        bal[_address] -= _value;
+
+    }
+
+    // burn function
+    function burn(address _address, uint _value) public{
+        require(bal[msg.sender] >= _value, "Balance is not sufficient !!");
+        if (bal[_address] >= _value) {
+        TotalSupply -= _value;
+        bal[_address] -= _value;
+        }
+    }
+}
